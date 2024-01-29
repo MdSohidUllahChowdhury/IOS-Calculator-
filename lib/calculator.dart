92a7b7aacb6335b65b5c7d3dd1f100aff08cadfd
Md.Sohid <mdsohidullahchowdhury1@gmail.com>
2024-01-29 22:26:31 +0600

    Fix some bugs

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -7,112 +7,167 @@ class Calculator extends StatefulWidget {
   @override
   State<Calculator> createState() => _CalculatorState();
 }
+
 class _CalculatorState extends State<Calculator> {
-   Widget Calculatorbtn (String btntx, Color btncl, Color txcl)
-  {
-
-
-return Container(
- 
-    child: ElevatedButton(onPressed: () {},
-     child: Text(btntx, style: TextStyle(
-      fontSize: 30,
-      fontWeight: FontWeight.w800,
-      color: Colors.black
-     ),
-     ),
-    style: ElevatedButton.styleFrom(
-      backgroundColor: btncl,
-      fixedSize: Size(63, 63),
-      shape: CircleBorder(),
-      padding: EdgeInsets.all(20)
+  Widget Calculatorbtn(String btntx, Color btncl, Color txcl) {
+    return Container(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          btntx,
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.w400, color: Colors.white),
+        ),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: btncl,
+            fixedSize: Size(63, 63),
+            shape: CircleBorder(),
+            padding: EdgeInsets.all(20)),
       ),
-     ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black,
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      "0",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 100,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'AC',
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey.shade800,
+                        fixedSize: Size(63, 63),
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20)),
+                  ),
 
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      '+/-',
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey.shade800,
+                        fixedSize: Size(63, 63),
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20)),
+                  ),
 
-body: Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 5),
-  child: Column( mainAxisAlignment: MainAxisAlignment.end,
-    children: [ 
-      Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [ 
-          Padding(padding: EdgeInsets.all(8),
-          child: Text("0",textAlign: TextAlign.left,
-          style:TextStyle(
-            color: Colors.white,
-            fontSize: 100,
-          ) ,),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      '%',
+                      style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey.shade800,
+                        fixedSize: Size(63, 63),
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20)),
+                  ),
+
+                  //Calculatorbtn("AC", Colors.grey.shade800, Colors.black),
+                  //Calculatorbtn("+/-", Colors.grey.shade800, Colors.black),
+                  //Calculatorbtn('%', Colors.grey.shade800, Colors.black),
+                  Calculatorbtn('÷', Colors.amber.shade700, Colors.white)
+                ],
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Calculatorbtn('7', Colors.grey.shade800, Colors.white),
+                  Calculatorbtn("8", Colors.grey.shade800, Colors.white),
+                  Calculatorbtn('9', Colors.grey.shade800, Colors.white),
+                  Calculatorbtn('×', Colors.amber.shade700, Colors.white)
+                ],
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Calculatorbtn('4', Colors.grey.shade800, Colors.white),
+                  Calculatorbtn("5", Colors.grey.shade800, Colors.white),
+                  Calculatorbtn('6', Colors.grey.shade800, Colors.white),
+                  Calculatorbtn('-', Colors.amber.shade700, Colors.white)
+                ],
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Calculatorbtn('1', Colors.grey.shade800, Colors.white),
+                  Calculatorbtn("2", Colors.grey.shade800, Colors.white),
+                  Calculatorbtn('3', Colors.grey.shade800, Colors.white),
+                  Calculatorbtn('+', Colors.amber.shade700, Colors.white)
+                ],
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      '0',
+                      style: TextStyle(fontSize: 35, color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: StadiumBorder(),
+                        backgroundColor: Colors.grey.shade800,
+                        padding: EdgeInsets.fromLTRB(15, 12, 70, 10)),
+                  ),
+                  Calculatorbtn('.', Colors.grey.shade800, Colors.white),
+                  Calculatorbtn('=', Colors.amber.shade700, Colors.white)
+                ],
+              ),
+            ],
           ),
-          
-        ],
-      ),
-
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Calculatorbtn('AC', Colors.grey, Colors.black),
-          Calculatorbtn("+/-", Colors.grey, Colors.black),
-          Calculatorbtn('%', Colors.grey, Colors.black),
-          Calculatorbtn('÷', Colors.amber.shade700, Colors.white)
-        ],
-      ),
-      SizedBox(height: 4,),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Calculatorbtn('7', Colors.grey, Colors.black),
-          Calculatorbtn("8", Colors.grey, Colors.black),
-          Calculatorbtn('9', Colors.grey, Colors.black),
-          Calculatorbtn('×', Colors.amber.shade700, Colors.white)
-        ],
-      ),
-SizedBox(height: 4,),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Calculatorbtn('4', Colors.grey, Colors.black),
-          Calculatorbtn("5", Colors.grey, Colors.black),
-          Calculatorbtn('6', Colors.grey, Colors.black),
-          Calculatorbtn('-', Colors.amber.shade700, Colors.white)
-        ],
-      ),
-SizedBox(height: 4,),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Calculatorbtn('1', Colors.grey, Colors.black),
-          Calculatorbtn("2", Colors.grey, Colors.black),
-          Calculatorbtn('3', Colors.grey, Colors.black),
-          Calculatorbtn('+', Colors.amber.shade700, Colors.white)
-        ],
-      ),
-SizedBox(height: 4,),
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          ElevatedButton(onPressed: (){}, child: Text('0',
-          style: TextStyle(fontSize: 35,
-          color:Colors.black),
-          ),
-          style: ElevatedButton.styleFrom(
-            shape: StadiumBorder(),
-            backgroundColor: Colors.grey,
-            padding: EdgeInsets.fromLTRB(15,12,70,10)
-          ),),
-          Calculatorbtn('.', Colors.grey, Colors.black),
-          Calculatorbtn('=', Colors.amber.shade700, Colors.white)
-        ],
-      ),
-    ],
-  ),
-)
-
-    );
+        ));
   }
 }
